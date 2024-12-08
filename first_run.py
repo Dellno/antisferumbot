f49e78d47868a251c9751c5528c3989282b9f390
Dellno <132328813+Dellno@users.noreply.github.com>
2024-12-08 12:08:12 +0500

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -1,2 +1,23 @@
-import aiogram
+from aiogram import Bot, Dispatcher
+import asyncio
+from aiogram.filters import Command
+
+# Инициализация
+
+bot_path = ""
+
+
+async def main():
+    dp = Dispatcher()
+    bot = Bot(bot_token)
+    tasks=[dp.start_polling(bot),auto_parse()]
+    await asyncio.gather(*tasks)
+
+@dp.message(Command("start"))
+async def start(message):
+    with open(bot_path + "bot_memory.txt", "w") as f1:
+        f1.write(str(message.chat.id))
+        f1.write("\n")
+        f1.write(str(msg_memory[-1]['conversation_message_id']))
+
 
